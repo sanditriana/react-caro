1e81c1c31911dc61441bb88d3f8f7cc7a4274cd4
unknown <AWDA@AWDA-FIN-006.maucash.local>
2024-12-31 18:10:00 +0700

    all

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { DarkThemeToggle } from "flowbite-react";
+import Carousel from "@/components/Carousel";
+
+export default function Home() {
+  return <Carousel />;
+}
+
+// export default function Home() {
+//   return <Carousel />;
+// }
